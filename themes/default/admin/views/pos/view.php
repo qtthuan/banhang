cf2334468f5583d0ddf86814b814628c527cec29
unknown <qtthuan2003@gmail.com>
2023-11-16 15:51:40 +0700

    fix1611 2

--- themes/default/admin/views/pos/view.php
+++ themes/default/admin/views/pos/view.php
@@ -136,7 +136,7 @@
                 }
                 echo "<p>";
                 ?>
-            <?php if ($inv->warehouse_id != 3 || ($inv->warehouse_id == 3 && $inv->customer_id != 1)) { ?>
+            <?php if ($inv->warehouse_id != 3 || ($inv->warehouse_id == 3 && $inv->customer != 'KHÁCH LẺ')) { ?>
                 <!--Nếu kho Ba-Ni hoặc (Tiệm Nước Mini và Có nhập tên) => Hiện Tên Khách-->
                 <?=lang("customer") . ': ' .  $inv->customer?><span style="color:red; font-size: 13px; margin-left: 15px;" class="no-print"><a href="#" id="view-customer" class="external pos-tip" title="<?=lang('customers_info');?>" data-toggle="modal" data-target="#myModal2"><i class="fa fa-bars" id="addIcon" style="font-size: 1.7em;"></i></a></span>
                 <br>
