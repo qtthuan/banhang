4e3a018267832efaf50e3bc1f116bb856a77ea6f
unknown <qtthuan2003@gmail.com>
2024-05-10 13:30:32 +0700

    fix 1005

--- themes/default/admin/views/pos/add.php
+++ themes/default/admin/views/pos/add.php
@@ -801,13 +801,13 @@
                     ?>
                         
                 </div>
-                <!--<div class="form-group">
+                <div class="form-group">
                     <?= lang('ordered', 'iordered'); ?>
                     <?php
                     $opts = array(0 => lang('no'), 1 => lang('yes'));
                     ?>
                     <?= form_dropdown('ordered', $opts, '', 'class="form-control" id="iordered" style="width:100%;"'); ?>
-                </div>-->
+                </div>
                 <input type="hidden" id="irow_id" value=""/>
             </div>
             <div class="modal-footer">
