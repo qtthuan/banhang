3e4e838eb9b7923e3c5ca0acf63a76135db1c418
unknown <qtthuan2003@gmail.com>
2026-08-21 20:41:16 +0700

    lap_2108_7

--- themes/default/admin/views/pos/mini.php
+++ themes/default/admin/views/pos/mini.php
@@ -2,6 +2,9 @@
 <!DOCTYPE html>
 <html lang="vi">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini POS</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
