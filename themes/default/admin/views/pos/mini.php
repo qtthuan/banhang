6583e25d866188c7626937cde852ad6dc8e24f35
unknown <qtthuan2003@gmail.com>
2026-08-25 14:58:14 +0700

    lap_2508_20

--- themes/default/admin/views/pos/mini.php
+++ themes/default/admin/views/pos/mini.php
@@ -22,7 +22,7 @@
     <link
         rel="stylesheet"
         type="text/css"
-        href="<?=$assets?>pos/css/mini.css?v=20260821-4"
+        href="<?=$assets?>pos/css/mini.css?v=20260821-5"
     >
 </head>
 <body>
